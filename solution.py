c2e07cceffc1426ab02d7688abfbded1a32a3fe7
MikhaelKreslavskii <116979503+MikhaelKreslavskii@users.noreply.github.com>
2023-04-07 20:52:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -3,10 +3,9 @@ import numpy as np
 
 
 chat_id = 613759661 # Ваш chat ID, не меняйте название переменной
-
 def solution(x: np.array) -> float:
     t=10
     n=len(x)
     v=x
-    a=2*((min(x)+41)/100)
+    a=2*((np.exp(len(x))+41)/100)
     return a # Ваш ответ
